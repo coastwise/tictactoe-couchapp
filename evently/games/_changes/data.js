8af1a2c087eab1fa617c3e679818c469a2139b14
Patrick McKenna <patrick.a.mckenna@gmail.com>
2011-11-06 07:29:00 -0500

    games _changes evently trigger

---
+++ evently/games/_changes/data.js
@@ -0,0 +1,13 @@
+function(data) {
+  $.log(data)
+  var p;
+  return {
+    games : data.rows.map(function(r) {
+      p = {};
+      p.opponent = r.key[1];
+      p.gameid = r.value._id;
+      p.turn = r.value._rev;
+      return p;
+    })
+  }
+};
---
+++ views/games/map.js
@@ -0,0 +1,5 @@
+function(doc) {
+  if (doc.players) {
+    emit([doc.players[0], doc.players[1]], doc);
+  }
+};
