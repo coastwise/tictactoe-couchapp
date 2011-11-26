694e7c8915a53a560c093d31a815de76231e0a39
Patrick McKenna <patrick.a.mckenna@gmail.com>
2011-11-25 21:15:17 -0500

    draw board

--- _attachments/script/tictactoe.js
+++ _attachments/script/tictactoe.js
@@ -9,9 +9,39 @@ $(function() {
 		db.openDoc(gameDocId, {
 			success : function(gameDoc) {
 				console.log("successfully loaded game " + gameDocId);
+				drawBoard();
 			}
 		});
 	}
 	$('#game').bind('loadGame', loadGame);
+	
+	function drawBoard() {
+		// insert the canvas
+		$('#game').html($("#board-template").html());
+		
+		var canvas = document.getElementById('board');
+		var width = canvas.width;
+		var height = canvas.height;
+		
+		var context = canvas.getContext('2d');
+		
+		context.beginPath();
+		context.strokeStyle = '#000';
+		context.lineWidth   = 4;
 
+		context.moveTo((width / 3), 0);
+		context.lineTo((width / 3), height);
+
+		context.moveTo((width / 3) * 2, 0);
+		context.lineTo((width / 3) * 2, height);
+
+		context.moveTo(0, (height / 3));
+		context.lineTo(width, (height / 3));
+
+		context.moveTo(0, (height / 3) * 2);
+		context.lineTo(width, (height / 3) * 2);
+
+		context.stroke();
+		context.closePath();
+	}
 });
